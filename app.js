4fe114faa97658ad4506644912b4ff19279a9994
Heldrin <altrionyx@protonmail.com>
2025-11-07 15:35:17 -0500

    Change port to work on PaaS

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Localhost Port
-PORT = 3000;
+PORT = 8000;
 
 const navLinks = [
   { href: "/", text: "Message Board" },
